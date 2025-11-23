2e1d77f8d222055750c619ef08f7a0643f185085
Lorenzo Zanoni <lorenzo.zanoni3@studio.unibo.it>
2025-11-23 16:39:55 +0100

    ex 03.01 done(controller)

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,34 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * a controller interface.
  */
 public interface Controller {
+    /**
+     * Sets the next String to be printed. if null, throws an exception.
+     * 
+     * @param data The string to be set
+     */
+    void setNext(String data);
 
+    /**
+     * Gets the next String to be printed.
+     * 
+     * @return the String to be printed
+     */
+    String getNext();
+
+    /**
+     * Gets the history of all printed strings.
+     * 
+     * @return the history of printed Strings
+     */
+    List<String> getHistory();
+
+    /**
+     * Prints the current string.
+     */
+    void printCurrent();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,59 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * 
- *
+ * A controller that manages I/O operations with strings.
  */
 public final class SimpleController implements Controller {
+    private final List<String> history = new LinkedList<>();
+    private String next;
 
+    /**
+     * A method to set the next string to print.
+     * 
+     * @param data the string to set as next
+     */
+    @Override
+    public void setNext(final String data) {
+        if (data != null) {
+            this.next = data;
+        } else {
+            throw new IllegalArgumentException("Data cannot be null");
+        }
+    }
+
+    /**
+     * A method to get the next string to print.
+     * 
+     * @return the next string
+     */
+    @Override
+    public String getNext() {
+        return this.next;
+    }
+
+    /**
+     * A method to get the history of printed strings.
+     * 
+     * @return the history
+     */
+    @Override
+    public List<String> getHistory() {
+        return history;
+    }
+
+    /**
+     * A method to get the current string to print.
+     */
+    @Override
+    public void printCurrent() {
+        try {
+            System.out.println(this.next); // NOPMD
+            this.history.add(this.next);
+        } catch (final IllegalStateException e) {
+            throw new IllegalStateException("No data to print", e);
+        }
+    }
 }
